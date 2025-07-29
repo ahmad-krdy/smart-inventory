625c099d2c8d8e57916d1d492537af4ef90d123d
Ahmad Abbas <ahmadabbas.krd@gmail.com>
2025-07-30 01:13:02 +0530

    updated

--- client/src/routes/AppRoute.js
+++ client/src/routes/AppRoute.js
@@ -1,28 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Alertdashboard from '../pages/Alertdashboard';
 import Product from '../pages/Product';
-// import NotAccessible from "../pages/NotAccessible.jsx"
 
 
 export default function AppRoutes() {
 
   return (
       <Routes>
-        {/* <Route path="/products" element={
-          <PublicRoute>
-            <Signin />
-          </PublicRoute>
-        } /> */}
-
         <Route path="/inventory-alert" element={ <Alertdashboard /> } />
-         <Route path="/product" element={ <Product /> } />
-
-{/*
-        <Route path="/not-accessible" element={
-          <PrivateRoute>
-            <NotAccessible />
-          </PrivateRoute>
-        } /> */}
+        <Route path="/product" element={ <Product /> } />
       </Routes>
   );
 }
